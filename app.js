677e843e1f00149f47e547d40f213c2adbf29ce6
StylishCS <118624912+StylishCS@users.noreply.github.com>
2024-07-24 02:34:45 +0300

    maybe round two

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ mongoose
 var app = express();
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://ieeehsb.software"],
+    origin: ["https://ieeehsb.software", "http://127.0.0.1:3000"],
+    credentials: true,
   })
 );
 // view engine setup
